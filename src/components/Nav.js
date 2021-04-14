03122480638c22b9ad74ed5efb80814584f2b1eb
Vishal Thakur <interaction.vishal@gmail.com>
2021-04-14 17:56:55 +0530

    File structure for portfolio

---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// Style
+import styled from 'styled-components';
+
+const Nav = () => {
+  return <div></div>;
+};
+
+const mainNav = styled(motion.div)``;
+
+export default Nav;
