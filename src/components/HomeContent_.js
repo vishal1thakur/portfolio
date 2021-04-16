0eeac7c5432235470ad234e922f2f2fe3a8d7256
Vishal Thakur <interaction.vishal@gmail.com>
2021-04-16 20:50:41 +0530

    Social link styles in homepage

--- src/components/HomeContent_.js
+++ src/components/HomeContent_.js
@@ -2,7 +2,6 @@ import React from 'react';
 // Images
 import github from '../img/github.svg';
 import linkedin from '../img/linkedin.svg';
-import resume from '../img/resume.svg';
 // Style
 import styled from 'styled-components';
 // Animation
@@ -40,37 +39,34 @@ const HomeContent = () => {
           Netlify
         </p>
       </Banner>
-      <Social>
-        <div className="box popup">
-          <h4 className="popup-name">GITHUB</h4>
-        </div>
-        <div className="box"></div>
-        <div className="box"></div>
+      <Meta>
+        <Social>
+          <div className="box">
+            <div className="empty"></div>
+          </div>
 
-        <div className="box github-box">
-          <div className="github">
-            <div className="git">
-              <img className="git-image" src={github} alt="" />
+          <div className="box github-box">
+            <div className="github">
+              <div className="git">
+                <img className="git-image" src={github} alt="" />
+              </div>
             </div>
           </div>
-        </div>
+        </Social>
+        <Social>
+          <div className="box">
+            <div className="empty"></div>
+          </div>
 
-        <div className="box github-box ">
-          <div className="github ">
-            <div className="git linkedin">
-              <img className="git-image" src={linkedin} alt="" />
+          <div className="box github-box">
+            <div className="github">
+              <div className="git">
+                <img className="git-image" src={github} alt="" />
+              </div>
             </div>
           </div>
-        </div>
-
-        <div className="box github-box ">
-          <div className="github">
-            <div className="git resume">
-              <img className="git-image" src={resume} alt="" />
-            </div>
-          </div>
-        </div>
-      </Social>
+        </Social>
+      </Meta>
     </StyledHomeContent>
   );
 };
@@ -81,14 +77,15 @@ const StyledHomeContent = styled(motion.div)`
   flex-wrap: wrap;
   margin-left: 100px;
   height: 100vh;
+  background-color: #12181b;
   overflow: hidden;
   color: #ffffff;
 `;
 
 const Banner = styled(motion.div)`
-  margin-left: 1.5rem;
+  margin-left: 3rem;
   padding: 10rem 10rem 20rem 10rem;
-  width: 53rem;
+  width: 50rem;
 
   h3 {
     font-size: 14px;
@@ -101,7 +98,7 @@ const Banner = styled(motion.div)`
   p {
     font-size: 15px;
     line-height: 160%;
-    letter-spacing: 0.09em;
+    letter-spacing: 0.03em;
     text-align: justify;
     color: #636566;
     padding-top: 3rem;
@@ -120,7 +117,7 @@ const Banner = styled(motion.div)`
   .copyright {
     padding-top: 0.5rem;
     text-align: center;
-    font-size: 11px;
+    font-size: 12px;
     opacity: 0.6;
   }
 `;
@@ -132,7 +129,7 @@ const Button = styled(motion.div)`
     display: block;
     text-align: center;
     text-decoration: none;
-    border: 2px solid rgba(255, 205, 46, 0.1);
+    border: 0.05rem solid rgba(255, 205, 46, 0.1);
     margin: auto;
     padding: 30px 10px;
     width: 100%;
@@ -178,7 +175,7 @@ const Name = styled(motion.div)`
   font-weight: bold;
   font-size: 54px;
   line-height: 127%;
-  letter-spacing: 0.12em;
+  letter-spacing: 0.04em;
   width: 100%;
   color: #ffffff;
 `;
@@ -193,7 +190,7 @@ const Skill = styled(motion.div)`
   font-size: 16px;
   line-height: 127%;
 
-  letter-spacing: 1.1em;
+  letter-spacing: 0.9em;
 
   color: #ffcd2e;
 
@@ -202,85 +199,74 @@ const Skill = styled(motion.div)`
   }
 `;
 
+const Meta = styled(motion.div)`
+  margin-left: 1px;
+`;
+
 const Social = styled(motion.div)`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-template-rows: 1.6fr 0.4fr;
-  gap: 0px 0px;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr 1fr;
+  gap: 14px 14px;
   grid-template-areas:
-    '. . .'
-    '. . .';
-  margin-left: -4rem;
+    '.'
+    '.';
+  width: 0.51rem;
   height: 100vh;
-  padding-right: 1000px;
-  p .box {
+
+  .box {
     /* background: white;
     border: 1px solid black;
     color: black; */
-  }
-
-  .popup {
-    position: relative;
-
-    .popup-name {
-      position: absolute;
-      bottom: -10%;
-      margin-left: 30px;
-      margin-bottom: 8px;
-      padding: 1px 5px;
-      font-size: 14px;
-      font-weight: 400;
-      background: rgba(255, 46, 46, 1);
-      border-radius: 3px;
-      display: none;
-    }
   }
 
   .github {
     width: 100%;
     height: 100%;
     display: flex;
-
     .git {
-      width: 100%;
+      width: 20%;
       height: 100%;
       border-radius: 100px 100px 0px 0px;
-      border: 2px dotted rgba(255, 46, 46, 0.3);
+      border: 3px solid rgba(235, 27, 12, 0.15);
       border-bottom: none;
-      margin-right: 35px;
-
+      margin-right: 30px;
+      margin-top: 40px;
       margin-left: 10px;
 
       cursor: pointer;
-      .git:hover {
-        background: rgba(255, 46, 46, 1);
-        .popup-name {
-          position: absolute;
-          bottom: 0;
-          margin-left: 30px;
-          margin-bottom: 8px;
-          padding: 1px 5px;
-          font-size: 14px;
-          font-weight: 400;
-          background: rgba(255, 46, 46, 1);
-          border-radius: 3px;
-          display: block;
-        }
-      }
       .git-image {
         display: flex;
         align-items: center;
         text-align: center;
         justify-content: center;
-        padding: 36px;
+        padding: 32px;
         opacity: 0.7;
       }
     }
-    .linkedin {
-      border: 2px dotted rgba(46, 117, 255, 0.3);
-    }
-    .resume {
-      border: 2px dotted rgba(255, 205, 46, 0.3);
+  }
+  .linkedin {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    .in {
+      width: 100%;
+      height: 135%;
+      border-radius: 0px 0px 100px 100px;
+      border: 3px solid rgba(46, 117, 255, 0.15);
+      border-top: none;
+      margin-right: 30px;
+      margin-bottom: -20px;
+
+      cursor: pointer;
+      .in-image {
+        padding-top: 300px;
+        padding-top: -300px;
+
+        margin-bottom: -20px;
+
+        opacity: 0.7;
+      }
     }
   }
 `;
